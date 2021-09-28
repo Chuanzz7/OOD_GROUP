c8be03ad2b0aae9c5ad217994795e2a1651d5723
Frost4465 <thomasquah4465@gmail.com>
2021-09-28 22:14:05 +0800

    THANK YOU FOREVER

---
+++ ood_assignment/src/main/java/GeneratePDF.java
@@ -0,0 +1,37 @@
+import java.io.FileNotFoundException;  
+import java.io.FileOutputStream;  
+import com.itextpdf.text.Document;  
+import com.itextpdf.text.DocumentException;  
+import com.itextpdf.text.Paragraph;  
+import com.itextpdf.text.pdf.PdfWriter; 
+
+
+
+public class GeneratePDF {
+    
+    public static void main(String args[])  {
+    Document doc = new Document();
+    
+           
+       
+    try 
+    {
+    PdfWriter writer = new PdfWriter.getInstance(doc, new FileOutputStream("C:\\Users\\Quah Jin Min\\Desktop\\COO"));
+    System.out.println("PDF created");
+    doc.open();
+    doc.add(new Paragraph("Chuan is GAY"));
+    doc.close();
+    writer.close();
+    }
+    catch(DocumentException e)
+    {
+        e.printStackTrace();
+    }
+    catch(FileNotFoundException e)
+    {
+        e.printStackTrace();
+    }
+}
+}
+    
+
