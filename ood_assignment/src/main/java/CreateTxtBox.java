4e47c811e6c8a0e599b55f20c342594937136a42
Frost4465 <thomasquah4465@gmail.com>
2021-09-29 00:33:52 +0800

    THANK YOU FOREVER I AM DIE

--- ood_assignment/src/main/java/CreateTxtBox.java
+++ ood_assignment/src/main/java/CreateTxtBox.java
@@ -1,12 +1,13 @@
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 
 public class CreateTxtBox {
     
     
-public void txt(String item_name, double item_price, int item_Quantity) {
+public void CreateFile() {
     
 try{
             File createFile = new File("C:\\Users\\Quah Jin Min\\Desktop\\OOD\\Chuan.txt"); //input file 
@@ -20,6 +21,20 @@ try{
             System.out.println("CHUAN GAYNESS MADE ERROR");
             e.printStackTrace();
         }
+}
+
+public void WriteFile(){
+    try{
+        FileWriter writeFile = new FileWriter("C:\\Users\\Quah Jin Min\\Desktop\\OOD\\Chuan.txt");
+        writeFile.write("Chuan is very GAY");
+        writeFile.close();
+        System.out.println("Wrote Chuan is GAY");
+        
+    }catch(IOException e)
+    {
+        System.out.println("Gayness made ERROR");
+        e.printStackTrace();
+    }
 }
     
 }
