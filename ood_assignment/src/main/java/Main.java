8d9563370fa0512eab3f122ca0c049f2161d2aee
Chuanzz7 <91423566+Chuanzz7@users.noreply.github.com>
2021-09-28 23:12:32 +0800

    file create

--- ood_assignment/src/main/java/Main.java
+++ ood_assignment/src/main/java/Main.java
@@ -1,20 +1,21 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 
 /**
  *
  * @author chngk
  */
-import java.util.ArrayList;
 public class Main {
-    public static void main(String[] args) {     
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
         
-       cartUI start = new cartUI();
-     }
+        cartUI start = new cartUI();
     }
     
-    
-    
-
+}
